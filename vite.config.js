476996befc26327998fad34e8134ab11bda64ff9
SEO User <seo_user@example.com>
2025-05-02 23:06:31 -0400

    se sube mejoras al server

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/', // ⚠️ Esto asegura que las rutas JS/CSS sean absolutas y no se rompan
   plugins: [vue()]
 })
