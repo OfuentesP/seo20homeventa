44a5eec1b965685c038ebcfcb774bc7e75eb23bd
SEO User <seo_user@example.com>
2025-05-07 15:26:26 -0400

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://seo20.dev',
   trailingSlash: 'always',
-  output: 'static', // 👈 modo estático real
-  build: {
-    format: 'directory'
-  },
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [
     tailwind(),
     vue()
   ],
+  build: {
+    format: 'directory'
+  },
   vite: {
     optimizeDeps: {
       exclude: ['@astrojs/tailwind']
